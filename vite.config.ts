d27f67f4d74150c9eda6fd3f0db784a0a00971c2
Ankit Kumar <ankitkumar@Ankits-MacBook-Air.local>
2025-09-23 18:14:02 +0530

    updated base url to /

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ankitgeotek.github.io/', // This should match your repo name
+  base: '/', // This should match your repo name
   build: {
     outDir: 'dist',
     rollupOptions: {
