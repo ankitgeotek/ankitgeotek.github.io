0dded5bfe044f94e082ef5cb91b3fae204fd5076
Ankit Kumar <ankitkumar@Ankits-MacBook-Air.local>
2025-09-23 17:31:32 +0530

    chk-1

---
+++ dist/assets/motion-DZa3STBg.js
@@ -0,0 +1,1 @@
+import"./mui-core-BdbV3Eyz.js";
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,24 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/ankitgeotek.github.io/', // This should match your repo name
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'mui-core': ['@mui/material'],
+          'mui-icons': ['@mui/icons-material'],
+          'mui-emotion': ['@emotion/react', '@emotion/styled'],
+          'form-libs': ['react-hook-form', '@hookform/resolvers', 'yup'],
+          'chart-libs': ['recharts'],
+          'motion': ['framer-motion'],
+          'utils': ['axios', 'react-intersection-observer', 'react-router-dom']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 600
   },
   optimizeDeps: {
     include: ['@mui/material', '@emotion/react', '@emotion/styled']
